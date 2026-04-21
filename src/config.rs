209df44942a369f5898d055eb9f8dc13b4edc81c
tt523-cpu <132032280+tt523-cpu@users.noreply.github.com>
2026-04-21 18:56:36 +0800

    Update rendezvous server and public key constants

--- src/config.rs
+++ src/config.rs
@@ -156,9 +156,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
-
+pub const RENDEZVOUS_SERVERS: &[&str] = &["154.86.30.100"];
+pub const RS_PUB_KEY: &str = "6Ig7oM+RDZTh+j1QNR36cckXXyYnqcdetxcLdRIhAns=";
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
 pub const WS_RENDEZVOUS_PORT: i32 = 21118;
